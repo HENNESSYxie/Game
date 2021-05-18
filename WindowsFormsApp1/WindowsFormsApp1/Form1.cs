c18e5387a56dcb737f4052c99d3e6d0f2e5e32ce
HENNESSYxie <hennessyxie@gmail.com>
2021-05-18 09:32:02 +0500

    Battle Ground and some new Fields for Player

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -12,10 +12,65 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        Random rnd = new Random();
+        List<Image> list = new List<Image>() { Properties.Resources.Attack, Properties.Resources.Red,
+        Properties.Resources.Defense,Properties.Resources.Blue,
+        Properties.Resources.Yellow,Properties.Resources.Defense,
+        Properties.Resources.Red, Properties.Resources.Red,
+        Properties.Resources.Empty,Properties.Resources.Empty,
+        Properties.Resources.Attack, Properties.Resources.Attack,
+        Properties.Resources.Defense,Properties.Resources.Defense,
+        Properties.Resources.Green,Properties.Resources.Green,
+        Properties.Resources.Blue, Properties.Resources.Clones, 
+        Properties.Resources.Clones, Properties.Resources.Attack,
+        Properties.Resources.Attack, Properties.Resources.Defense,
+        Properties.Resources.Defense, Properties.Resources.Tripple_Damage,
+        Properties.Resources.Tripple_Damage,Properties.Resources.Yellow,
+        Properties.Resources.Defense,Properties.Resources.Defense,
+        Properties.Resources.Clones, Properties.Resources.Defense,
+         Properties.Resources.Red,Properties.Resources.Clones,
+        Properties.Resources.Green,Properties.Resources.Green,
+        Properties.Resources.Defense,Properties.Resources.Attack ,
+        };
+        Button first, second;
         public Form1()
         {
             InitializeComponent();
-          
+            PlaceImagesToButtonsSecond();
+            PlaceImagesToButtons();
+        }
+        private void PlaceImagesToButtonsSecond()
+        {
+            var lst = new List<Image>(list);
+            Button button;
+            int randomNumber;
+            for (int i = 0; i < tableLayoutPanel2.Controls.Count; i++)
+            {
+                if (tableLayoutPanel2.Controls[i] is Button)
+                    button = (Button)tableLayoutPanel2.Controls[i];
+                else
+                    continue;
+                randomNumber = rnd.Next(0, lst.Count);
+                button.BackgroundImage = lst[randomNumber];
+                lst.RemoveAt(randomNumber);
+                
+            }
+        }
+
+        private void PlaceImagesToButtons()
+        {
+            Button button;
+            int randomNumber;
+            for(int i=0;i<tableLayoutPanel1.Controls.Count;i++)
+            {
+                if (tableLayoutPanel1.Controls[i] is Button)
+                    button = (Button)tableLayoutPanel1.Controls[i];
+                else
+                    continue;
+                randomNumber = rnd.Next(0, list.Count);
+                button.BackgroundImage = list[randomNumber];
+                list.RemoveAt(randomNumber);
+            }
         }
 
       
--- WindowsFormsApp1/WindowsFormsApp1/Player.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Player.cs
@@ -8,6 +8,21 @@ namespace WindowsFormsApp1
 {
     public class Player
     {
-
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int AttackLevel { get; set; }
+        public int DefenseLevel { get; set; }
+        public Player(string name, int health = 100)
+        {
+            Name = name;
+            Health = health;
+        }
+        public Player(string name, int health = 100, int defense = 1, int attack = 1)
+        {
+            Name = name;
+            Health = health;
+            AttackLevel = attack;
+            DefenseLevel = defense;
+        }
     }
 }
